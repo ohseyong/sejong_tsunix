dd7ecbec3cbf3964e139708e7599e4d2f3e6c5bd
오세용 <alaala3@gmail.com>
2020-10-13 15:35:48 +0900

    1013

---
+++ week07/14011109-1.c
@@ -0,0 +1,27 @@
+nt main(void){
+	 int i, status;
+	  pid_t pid;
+	   for (i=0; i<3; i++){
+		    // child process 생성
+			//  if (pid==0&&i==0){
+			//   // 첫 번째 child가 새로운 프로그램 실행 시작
+			//   exit(0);
+			//    }
+			//     else if (pid==0&&i==1){
+			//      // 두 번째 child가 새로운 프로그램 실행 시작
+			//      exit(0);
+			//       }
+			//        else if (pid==0){
+			//         // 세 번째 child가 새로운 프로그램 실행 시작
+			//         exit(0);
+			//          }
+			//           }
+			//            for (i=0; i<3; i++){
+			//             // 세 child의 종료 상태를 종료 순서대로 확인 & 출력
+			//              }
+			//               exit(0);
+			//               }
+			//            }
+			//     }
+	   }
+}
