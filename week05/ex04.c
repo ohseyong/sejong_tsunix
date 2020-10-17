e754b1db499b70ed5af368a8a6f41f7f94d6b0ae
seoh <seoh@student.42seoul.kr>
2020-10-18 04:07:45 +0900

    Update ex04.c

--- week05/ex04.c
+++ week05/ex04.c
@@ -1,3 +1,5 @@
+// 4장 1강 + 5장 1강 52분 예제
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/types.h>
@@ -8,18 +10,18 @@ int main(void)
 	pid_t pid;
 
 	printf("parent\ngetpid = %ld / getppid = %ld\n", getpid(), getppid());
-	printf("getpid = %ld / getpgid = %ld\n", getpgrp(), getpgid(0));
+	printf("getpgrp = %ld / getpgid = %ld\n", getpgrp(), getpgid(0));
 
 	pid = fork();
 	if (pid == 0)
 	{
 		printf("child before \ngetpid = %ld / getppid = %ld\n", getpid(), getppid());
-		printf("getpid = %ld / getpgid = %ld\n", getpgrp(), getpgid(0));
+		printf("getpgrp = %ld / getpgid = %ld\n", getpgrp(), getpgid(0));
 
 		setpgid(getpid(), getpid());
 
-		printf("child after %ld : %ld\n", getpid(), getppid());
-		printf("child after %ld vs %ld\n", getpgrp(), getpgid(0));
+		printf("child after\ngetpid = %ld / getppid = %ld\n", getpid(), getppid());
+		printf("getpgrp = %ld / getpgid = %ld\n", getpgrp(), getpgid(0));
 	}
 	wait(0);
 	return 0;
