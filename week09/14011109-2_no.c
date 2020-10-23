1728f3a11d09ea20a81f41214548ca0994f1c5f1
오세용 <alaala3@gmail.com>
2020-10-23 13:56:03 +0900

    please checkgit add .

--- week09/14011109-2_no.c
+++ week09/14011109-2_no.c
@@ -26,12 +26,17 @@ void do_child(int i, int *cid)
 	int		j, status;
 	pid_t	pid;
 	// SIGUSR1 signal 처리가 가능 하도록 설정
+	///밑에 맞는지 봐주세열~~~~
 	static struct sigaction act;
 
 	act.sa_handler=justforsig;
 	sigaction(SIGINT, &act, NULL);
+	/////
+	
 	// 자신의 출력 순서가 될 때 까지 대기
-	pause();
+	//
+	//wait(&status); 나 pause(); 를 쓰는건가요??? 잘 모르겠습니다 ㅠㅠ
+
 	pid = getpid();
 	for (j = 0; j < 5; j++)
 	{
@@ -39,7 +44,9 @@ void do_child(int i, int *cid)
 		sleep(1);
 	}
 	// 출력 완료 후 다음 출력 순서의 child에게 출력 시작을 알림
-	kill(pid, SIGINT);
+	/// 밑에도 맞는지 봐주세열 ~~~~
+	kill(pid, SIGUSR1);
+	///
 	exit(0);
 }
 
